d9798603828457f0935c7c7451abd143e41c202c
Mercy Tryphen <memurugat@gmail.com>
2026-07-09 22:37:14 +0300

    fix: update layer_check.ts argument signature to fix ts build error

--- layer_check.ts
+++ layer_check.ts
@@ -8,7 +8,10 @@ console.log('Requirement: Agent must trace the error and identify L4 or L5 failu
 // Since we simulated a 10% packet loss in TransportModule, running this might succeed or fail.
 // To force failure, we can manually trigger the failure or just observe the logic.
 
-applicationLayer.sendMessage({ text: 'Ping!' }, 'user_A', 'user_B', 'session_789');
+applicationLayer.sendMessage({ text: 'Ping!' }, 'user_A', 'user_B', 'session_789', {
+  on_success: () => {},
+  on_failure: () => {}
+});
 
 setTimeout(() => {
   console.log('--- Test Finished ---');
